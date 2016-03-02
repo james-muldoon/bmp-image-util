de4400a9f6ce3cba7eeaaf73fbefff2e927ef83c
James Muldoon <u5796521@anu.edu.au>
2016-03-02 22:35:07 +1100

    Unable to make progress past segmentation error. Created furtherreading.txt to store helpful web links

--- bmpedit.c
+++ bmpedit.c
@@ -13,25 +13,20 @@ OPTIONS:
   -t 0.0-1.0   Apply a threshold filter to the image with a threshold the threshold value given.
   -h           Displays this usage message. */
 
-FILE *input_image;
-  
-// HEADER PARSER FUNCTION DEFINITION WILL GO HERE
-void HeaderParser(FILE input_image);
-
 
 /* Code for taking arguments from terminal appropriated from these sources:
 http://stackoverflow.com/questions/498320/pass-arguments-into-c-program-from-command-line
 http://stackoverflow.com/questions/17645447/how-to-pass-command-line-arguments-to-a-c-program?rq=1
 */
 int main(int argc, char *argv[]) {
-	input_image = fopen(argv[1], "r");
+  
 
-  HeaderParser(input_image);
+  FILE *input_image = fopen(argv[1], "r");
 
+  char header[54];
+  fread(&header, 54, 1, input_image);
 
 	printf("File name %s\n", argv[1]);
 	return 0;
-
-	// continue reading: http://faq.cprogramming.com/cgi-bin/smartfaq.cgi?answer=1048384537&id=1043284392
 
 }
