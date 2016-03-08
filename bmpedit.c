5e20d211f68f2e8ea5322edb35feae9c0e55f424
James Muldoon <u5796521@anu.edu.au>
2016-03-08 17:04:18 +1100

    completed threshold filter, now need to generalise code

--- bmpedit.c
+++ bmpedit.c
@@ -20,10 +20,14 @@ http://stackoverflow.com/questions/498320/pass-arguments-into-c-program-from-com
 http://stackoverflow.com/questions/17645447/how-to-pass-command-line-arguments-to-a-c-program?rq=1
 */
 int main(int argc, char *argv[]) {
-  int sz;
+  int sz, clrR, clrG, clrB;
+  float ratio, threshold; 
   unsigned char *data;
+  int i, j;
 
-  // read the file into memory, then close it
+  threshold = 0.5;
+
+  // read the file into memory
   FILE *input_image = fopen("cup.bmp", "r");
 
   if (input_image == NULL) {
@@ -40,7 +44,7 @@ int main(int argc, char *argv[]) {
   data = (unsigned char *) malloc(sz); 
 
   
-
+  // read the file into memory
   if (fread(data, sz, 1, input_image) != 1) {
     printf("Error: problem reading file\n");
     return 0;
@@ -48,23 +52,38 @@ int main(int argc, char *argv[]) {
 
   fclose(input_image);
   
+  // get information from the bmp header
   int width = data[18] | data[19] << 8 | data[20] << 16 | data[21] << 24;
   printf("width: %d\n", width);
-
   int height = data[22] | data[23] << 8 | data[24] << 16 | data[25] << 24;
   printf("height: %d\n", height);
 
 
   int offset = data[10] | data[11] << 8 | data[12] << 16 | data[13] << 24;
 
-  for (i=0;i<XDIM;i++) {  // set every pixel in the image based on bytes from the bmp file.
-       for (j=0;j<YDIM;j++) {
-           setcolorRGB(i, YDIM - j, data[pos + i*3 + j*XDIM*3 + 2], 
-                                    data[pos + i*3 + j*XDIM*3 + 1], 
-                                    data[pos + i*3 + j*XDIM*3 ]);   // note this striding will not work if the width is not a multiple of 4.
+  for (i=0;i<width;i++) {  
+       for (j=0;j<height;j++) {
+            clrR = data[offset + i*3 + j*width*3 + 2]; 
+            clrG = data[offset + i*3 + j*width*3 + 1]; 
+            clrB = data[offset + i*3 + j*width*3 ]; 
+
+            ratio = ((clrR + clrG + clrB) / 3 / 255.0f); 
+            
+            if (ratio < threshold) {
+              data[offset + i*3 + j*width*3 + 2] = 0;
+              data[offset + i*3 + j*width*3 + 1] = 0;
+              data[offset + i*3 + j*width*3    ] = 0;
+            } else {
+              data[offset + i*3 + j*width*3 + 2] = 255;
+              data[offset + i*3 + j*width*3 + 1] = 255;
+              data[offset + i*3 + j*width*3    ] = 255;
+            }
        }
     }
 
+    FILE *fp = fopen("test.bmp", "w+");
+    fwrite(data, 1, sz, fp);
+    fclose(fp);
 
 
   return 0;
